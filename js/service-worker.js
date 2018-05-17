15b7d6a564833dc9e69068088200a68a9225c99b
lockex1987 <lockex1987@gmail.com>
2018-05-17 22:17:03 +0700

    Test

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,12 +1,9 @@
-// Mỗi lần sửa file index, ảnh thì phải sửa cái này
-// Định dạng yyyymmdd-hhmi
-var cacheName = 'lichcuatoi-20180517-1505';
+var cacheName = 'lichcuatoi-20180517-1530';
 
-// '/images/favicon.png'
 var filesToCache = [
   '/',
-  //'/.',
   '/index.html',
+  '/images/favicon.png',
   '/css/calendar.css',
   '/js/calendar.js',
   "/lib/slick/slick.css",
@@ -16,22 +13,18 @@ var filesToCache = [
 ];
 
 self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
       return cache.addAll(filesToCache);
     })
   );
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
         if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
       }));
@@ -41,7 +34,6 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
